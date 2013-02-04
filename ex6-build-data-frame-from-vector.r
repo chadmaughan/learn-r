fccef9db49c6ab710bb75d3b00e8ee6093df609a
Chad Maughan <chadmaughan@gmail.com>
2013-02-03 22:13:12 -0700

    added building data.frame example

---
+++ ex6-build-data-frame-from-vector.r
@@ -0,0 +1,8 @@
+# create a vector with 'start:finish' notation
+w <- 18:23
+
+# create a vector with c() function which combines all arguments to form a vector
+h <- c(1.3, 2.8, 7.0, 0.9, 3.1, 0.2)
+
+# combine together to build a data frame
+data <- data.frame(width=w, height=h)
