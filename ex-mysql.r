e80850b7cf2b17158b604aa5a0a61e0ab8820e6d
Chad Maughan <chadmaughan@gmail.com>
2014-12-06 13:53:15 -0700

    Update ex-mysql.r

--- ex-mysql.r
+++ ex-mysql.r
@@ -1,3 +1,11 @@
+# a good tutorial: 
+#  http://www.r-bloggers.com/mysql-and-r/
+
+# coursera slides & video
+#  http://jtleek.com/modules/03_GettingData/02_01_readingMySQL/#1
+#  https://d396qusza40orc.cloudfront.net/getdata/lecture_slides/02_01_readingMySQL.pdf
+#  https://class.coursera.org/getdata-016/lecture/download.mp4?lecture_id=21
+
 # install the package before importing
 library(RMySQL)
 
